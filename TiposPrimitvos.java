ad43ccd89fede3b2e42bf2c0b3d3c5048cb13184
Ricardo2Silva <ricardojonasdasilva60@gmail.com>
2020-12-11 18:42:09 -0300

    initial commit

---
+++ TiposPrimitvos.java
@@ -0,0 +1,17 @@
+public class TiposPrimitivos {
+    public static void main(String[] args){
+
+        int idade = 29;
+        double preco = 12.45;
+        char sexo = "M";
+        boolean casado = false;
+
+        byte b = 127;
+        short  s = 32768;
+        int i = 2_000_000_000;
+        long l = 9_000_000_000_000_000_000;
+        double d =
+
+
+    }
+}
---
+++ Variavel.java
@@ -0,0 +1,6 @@
+public class Variavel {
+
+    public static void main(String[] args){
+        System.out.println("Texto");
+    }
+}
